5a2b9d8ba2928dffd697e91888a130bc72898c3a
Xuanwo <github@xuanwo.io>
2020-04-16 13:23:08 +0800

    api: Move all handlers to another package
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- api/handler.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import (
-	"net/http"
-)
-
-func Handler(w http.ResponseWriter, r *http.Request) {
-	router.ServeHTTP(w, r)
-}
--- api/init.go
+++ api/init.go
@@ -1,13 +1,20 @@
 package api
 
 import (
+	"net/http"
+
+	"github.com/Xuanwo/gitqus/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 var router *gin.Engine
 
+func Handler(w http.ResponseWriter, r *http.Request) {
+	router.ServeHTTP(w, r)
+}
+
 func init() {
 	router = gin.Default()
 
-	router.POST("/v0/comments/:provider/:user/:repo/:branch", CreateComment)
+	router.POST("/v0/comments/:provider/:user/:repo/:branch", handlers.CreateComment)
 }
